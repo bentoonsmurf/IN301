17c6775218f33bb4830636b1e95fb7f5c42305de
Your Name <you@example.com>
2015-10-15 12:49:09 +0200

    td2 fin de seance

--- td2/exo1.c
+++ td2/exo1.c
@@ -36,10 +36,42 @@ liste ajout_debut(liste l,int e){
 	tmp->suiv=l;
 	return tmp;
 	}
-/*int nb_ele(liste l){
-	return 0;
+/*int nb_ele(liste l){int n;n=0;
+    if (!test_vide(l)){n++;
+ 	nb_ele(l->suiv)
 }
 */
+liste ajout_fin(liste l,int e){
+	liste tmp=l;
+	if(test_vide) return ajout_debut(l,e);
+	while (tmp->suiv!=NULL){tmp=tmp->suiv;}
+	liste new=memory();
+	new->val=e; 
+	new->suiv=NULL;
+	tmp->suiv=new;
+	return l;
+}
+liste ajout_trie(liste l,int e){
+	liste tmp=l;
+	liste new=memory();
+	new->val=e;
+	if (l!=NULL || e<l->val){return ajout_debut(l,e);}
+	while (tmp->suiv==NULL && e>tmp->suiv->val){
+		tmp=tmp->suiv;}
+	new->suiv=tmp->suiv;
+	tmp->suiv=new;
+	return l;
+	
+}
+
+
+
+liste concat(liste l, liste m){
+	liste tmp=l;
+while (tmp->suiv!=NULL){tmp=tmp->suiv;}
+tmp->suiv=m;
+return l;
+}
 
 
 
@@ -50,4 +82,17 @@ l=ajout_debut(l,1);
 l=ajout_debut(l,2);
 l=ajout_debut(l,3);
 	affiche(l);
+
+liste m; m=NULL;
+m=ajout_debut(m,4);
+m=ajout_debut(m,5);
+m=ajout_debut(m,6);
+affiche(m);
+
+printf("\n");
+l=concat(l,m);
+//affiche(l);
+
+l=ajout_trie(l,3);
+affiche(l);
 }
