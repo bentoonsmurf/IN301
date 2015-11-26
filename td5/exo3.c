300a91562a04e441e59452acea71a7f7d7c55369
Your Name <you@example.com>
2015-11-26 12:41:36 +0100

    exercices de td5

---
+++ td5/exo3.c
@@ -0,0 +1,32 @@
+//i = bleu +haut  
+//j= rouge + coté
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+
+
+int main(){
+printf("P3\n");
+printf("256 256\n");
+printf("255\n");
+
+
+int i,j;
+
+for (i=0;i<256;i++){
+	for(j=0;j<256;j++){
+		printf("%d\n0\n%d\n",i,j);
+		
+	}
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
